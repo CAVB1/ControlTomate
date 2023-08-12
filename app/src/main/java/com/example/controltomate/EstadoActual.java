8b149c84f819396c54113402256576074a04ce3c
Carlos Villegas <2030072@upt.edu.mx>
2023-08-12 11:20:23 -0600

    Menú y actividades para las demás interfaces

---
+++ app/src/main/java/com/example/controltomate/EstadoActual.java
@@ -0,0 +1,14 @@
+package com.example.controltomate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EstadoActual extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_estado_actual);
+    }
+}
--- app/src/main/java/com/example/controltomate/MainActivity.java
+++ app/src/main/java/com/example/controltomate/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.controltomate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button estadoActual, historial, acciones;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        estadoActual=findViewById(R.id.btnEstadoActual);
+        historial=findViewById(R.id.btnHistorial);
+        acciones=findViewById(R.id.btnAcciones);
+
+        estadoActual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent currentState=new Intent(MainActivity.this,EstadoActual.class);
+                startActivity(currentState);
+            }
+        });
+
+        historial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent history=new Intent(MainActivity.this, Historial.class);
+                startActivity(history);
+            }
+        });
+
+        acciones.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent actions=new Intent(MainActivity.this, Acciones.class);
+            }
+        });
     }
 }
