b1a6b982c2a1ff94351a1464afc4bfa6de175efc
Carlos Villegas <2030072@upt.edu.mx>
2023-08-12 13:54:42 -0600

    Interfaz con botón para enviar la orden de Activar la bomba de agua

--- app/src/main/java/com/example/controltomate/Acciones.java
+++ app/src/main/java/com/example/controltomate/Acciones.java
@@ -3,12 +3,24 @@ package com.example.controltomate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Acciones extends AppCompatActivity {
-
+    private Button btnActivarBomba;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acciones);
+        btnActivarBomba = findViewById(R.id.btnActivarBomba);
+
+        // Configurar el evento clic del botón
+        btnActivarBomba.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Coloca aquí la lógica para activar la bomba
+                HTTPCommunication.PostRequest(new String[]{"codigo"}, new String[]{"1"},"registrarAccion.php",getApplicationContext(),"La acción se ha enviado","Ya has enviado esta acción");
+            }
+        });
     }
 }
