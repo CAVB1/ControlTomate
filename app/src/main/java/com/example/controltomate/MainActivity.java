60b045b580c16377fa85c76cd830d642d7d012c5
Carlos Villegas <2030072@upt.edu.mx>
2023-08-24 10:54:37 -0600

    Rediseño de la interfaz

--- app/src/main/java/com/example/controltomate/MainActivity.java
+++ app/src/main/java/com/example/controltomate/MainActivity.java
@@ -9,7 +9,11 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -22,9 +26,12 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
-    TextView tanque1, tanque2, humedad, ph;
+    TextView tanque1, tanque2, humedad, ph,statet;
     public static final String CHANNEL_ID = "MyChannel";
 Button estadoActual, historial, acciones;
+RelativeLayout bloque1, bloque2, bloque3, bloque4;
+private ImageView imaget;
+private ScrollView cont;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,16 +49,145 @@ Button estadoActual, historial, acciones;
 
         estadoActual=findViewById(R.id.btnEstadoActual);
         historial=findViewById(R.id.btnHistorial);
-        tanque1=findViewById(R.id.tanque1);
-        tanque2=findViewById(R.id.tanque2);
-        humedad=findViewById(R.id.humedad);
-        ph=findViewById(R.id.ph);
+        tanque1=findViewById(R.id.ttanque1);
+        tanque2=findViewById(R.id.ttanque2);
+        humedad=findViewById(R.id.thumedad);
+        ph=findViewById(R.id.tph);
+        bloque1=findViewById(R.id.block1);
+        bloque2=findViewById(R.id.block2);
+        cont=findViewById(R.id.maincon);
+        imaget=findViewById(R.id.imaget);
+        statet=findViewById(R.id.stateText);
+
+        imaget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                JsonArrayRequest jsonArrayRequest= new JsonArrayRequest(HTTPCommunication.SERVER_NAME + "getLastEstado.php", new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try {
+                            JSONObject object=(response.getJSONObject(0));
+                            if(object.getInt("NIVEL_AGUA")<=0){
+                                tanque1.setText(object.getString("NIVEL_AGUA")+"%");
+                                bloque1.setBackgroundResource(R.color.rojillo);
+                                bloque1.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        Toast.makeText(MainActivity.this, "Es necesario llenar el tanque 1\nEstá vacío", Toast.LENGTH_LONG).show();
+                                    }
+                                });
+                            }else if (object.getInt("NIVEL_AGUA")<10){
+                                tanque1.setText(object.getString("NIVEL_AGUA")+"%");
+                                bloque1.setBackgroundResource(R.color.rojillo);
+                                bloque1.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        Toast.makeText(MainActivity.this, "El tanque 1 se está quedando sin agua\nSe recomienda llenarlo pronto", Toast.LENGTH_LONG).show();
+                                    }
+                                });;
+
+                            }else if (object.getInt("NIVEL_AGUA")<50){
+                                tanque1.setText(object.getString("NIVEL_AGUA")+"%");
+                                bloque1.setBackgroundResource(R.color.rojillo);
+                                bloque1.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        Toast.makeText(MainActivity.this, "El agua está por debajo del 50%", Toast.LENGTH_LONG).show();
+                                    }
+                                });
+                            }else {
+                                tanque1.setText(object.getString("NIVEL_AGUA")+"%");
+                                bloque1.setBackgroundResource(R.color.greensh);
+                                bloque1.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        Toast.makeText(MainActivity.this, "EL nivel de agua en el tanque 1 es correcto", Toast.LENGTH_LONG).show();
+                                    }
+                                });
+
+
+                            }
+
+                            if(object.getInt("NIVEL_AGUA2")<=0){
+                                tanque2.setText(object.getString("NIVEL_AGUA2")+"%");
+                                bloque2.setBackgroundResource(R.color.rojillo);
+                                bloque2.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        Toast.makeText(MainActivity.this, "Es necesario llenar el tanque 2\nEstá vacío", Toast.LENGTH_LONG).show();
+                                    }
+                                });
+                            }else if (object.getInt("NIVEL_AGUA2")<10){
+                                tanque2.setText(object.getString("NIVEL_AGUA2")+"%");
+                                bloque2.setBackgroundResource(R.color.rojillo);
+                                bloque2.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        Toast.makeText(MainActivity.this, "El tanque 2 se está quedando sin agua\nSe recomienda llenarlo pronto", Toast.LENGTH_LONG).show();
+                                    }
+                                });;
+
+                            }else if (object.getInt("NIVEL_AGUA2")<50){
+                                tanque2.setText(object.getString("NIVEL_AGUA2")+"%");
+                                bloque2.setBackgroundResource(R.color.rojillo);
+                                bloque2.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        Toast.makeText(MainActivity.this, "El agua está por debajo del 50%", Toast.LENGTH_LONG).show();
+                                    }
+                                });
+                            }else {
+                                tanque2.setText(object.getString("NIVEL_AGUA2")+"%");
+                                bloque2.setBackgroundResource(R.color.greensh);
+                                bloque2.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        Toast.makeText(MainActivity.this, "EL nivel de agua en el tanque 2 es correcto", Toast.LENGTH_LONG).show();
+                                    }
+                                });
+                            }
+
+                            humedad.setText(object.getString("NIVEL_HUMEDAD")+"%");
+
+                            ph.setText(object.getString("NIVEL_PH")+"%");
+
+                            if((object.getInt("NIVEL_AGUA")<50&&object.getInt("NIVEL_AGUA2")<50)||object.getInt("NIVEL_AGUA")<=10||object.getInt("NIVEL_AGUA2")<=10){
+                                cont.setBackgroundResource(R.drawable.orangeborder);
+                                imaget.setImageResource(R.drawable.sad);
+                                statet.setText("Estado\nLa planta necesita atención");
+
+                            }else{
+                                cont.setBackgroundResource(R.drawable.border);
+                                imaget.setImageResource(R.drawable.japy);
+
+                                statet.setText("Estado\nLa planta está bien");
+                            }
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+
+                RequestQueue requestQueue= Volley.newRequestQueue(getApplicationContext());
+                requestQueue.add(jsonArrayRequest);
+            }
+        });
+
 
         estadoActual.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent currentState=new Intent(MainActivity.this,EstadoActual.class);
-                startActivity(currentState);
+               /* Intent currentState=new Intent(MainActivity.this,EstadoActual.class);
+                startActivity(currentState);*/
+                Intent ver=new Intent(MainActivity.this, VerPlanta.class);
+                startActivity(ver);
             }
         });
 
@@ -71,32 +207,100 @@ Button estadoActual, historial, acciones;
                 try {
                     JSONObject object=(response.getJSONObject(0));
                     if(object.getInt("NIVEL_AGUA")<=0){
-                        tanque1.setText("Tanque 1\n Estado: Rellenar agua (0% nivel de agua)");
+                        tanque1.setText(object.getString("NIVEL_AGUA")+"%");
+                        bloque1.setBackgroundResource(R.color.rojillo);
+                        bloque1.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Toast.makeText(MainActivity.this, "Es necesario llenar el tanque 1\nEstá vacío", Toast.LENGTH_LONG).show();
+                            }
+                        });
                     }else if (object.getInt("NIVEL_AGUA")<10){
-                        tanque1.setText("Tanque 1:\n Estado: Rellenar agua (<10% nivel de agua)");
+                        tanque1.setText(object.getString("NIVEL_AGUA")+"%");
+                        bloque1.setBackgroundResource(R.color.rojillo);
+                        bloque1.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Toast.makeText(MainActivity.this, "El tanque 1 se está quedando sin agua\nSe recomienda llenarlo pronto", Toast.LENGTH_LONG).show();
+                            }
+                        });;
 
                     }else if (object.getInt("NIVEL_AGUA")<50){
-                        tanque1.setText("Tanque 1:\n Estado: Se está quedando sin agua (<50% nivel de agua)");
+                        tanque1.setText(object.getString("NIVEL_AGUA")+"%");
+                        bloque1.setBackgroundResource(R.color.rojillo);
+                        bloque1.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Toast.makeText(MainActivity.this, "El agua está por debajo del 50%", Toast.LENGTH_LONG).show();
+                            }
+                        });
                     }else {
-                        tanque1.setText("Tanque 1:\n Estado: Niveles de agua correctos (>=50% nivel de agua)");
+                        tanque1.setText(object.getString("NIVEL_AGUA")+"%");
+                        bloque1.setBackgroundResource(R.color.greensh);
+                        bloque1.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Toast.makeText(MainActivity.this, "EL nivel de agua en el tanque 1 es correcto", Toast.LENGTH_LONG).show();
+                            }
+                        });
 
 
                     }
 
                     if(object.getInt("NIVEL_AGUA2")<=0){
-                        tanque2.setText("Tanque 2\n Estado: Rellenar agua (0% nivel de agua)");
+                        tanque2.setText(object.getString("NIVEL_AGUA2")+"%");
+                        bloque2.setBackgroundResource(R.color.rojillo);
+                        bloque2.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Toast.makeText(MainActivity.this, "Es necesario llenar el tanque 2\nEstá vacío", Toast.LENGTH_LONG).show();
+                            }
+                        });
                     }else if (object.getInt("NIVEL_AGUA2")<10){
-                        tanque2.setText("Tanque 2:\n Estado: Rellenar agua (<10% nivel de agua)");
+                        tanque2.setText(object.getString("NIVEL_AGUA2")+"%");
+                        bloque2.setBackgroundResource(R.color.rojillo);
+                        bloque2.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Toast.makeText(MainActivity.this, "El tanque 2 se está quedando sin agua\nSe recomienda llenarlo pronto", Toast.LENGTH_LONG).show();
+                            }
+                        });;
 
                     }else if (object.getInt("NIVEL_AGUA2")<50){
-                        tanque2.setText("Tanque 2:\n Estado: Se está quedando sin agua (<50% nivel de agua)");
+                        tanque2.setText(object.getString("NIVEL_AGUA2")+"%");
+                        bloque2.setBackgroundResource(R.color.rojillo);
+                        bloque2.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Toast.makeText(MainActivity.this, "El agua está por debajo del 50%", Toast.LENGTH_LONG).show();
+                            }
+                        });
                     }else {
-                        tanque2.setText("Tanque 2:\n Estado: Niveles de agua correctos (>=50% nivel de agua)");
+                        tanque2.setText(object.getString("NIVEL_AGUA2")+"%");
+                        bloque2.setBackgroundResource(R.color.greensh);
+                        bloque2.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Toast.makeText(MainActivity.this, "EL nivel de agua en el tanque 2 es correcto", Toast.LENGTH_LONG).show();
+                            }
+                        });
                     }
 
-                    humedad.setText("Humedad de la planta: "+object.getString("NIVEL_HUMEDAD")+"%\nEstado: Normal");
+                    humedad.setText(object.getString("NIVEL_HUMEDAD")+"%");
 
-                    ph.setText("PH de la planta: "+object.getString("NIVEL_PH")+"%");
+                    ph.setText(object.getString("NIVEL_PH")+"%");
+
+                    if((object.getInt("NIVEL_AGUA")<50&&object.getInt("NIVEL_AGUA2")<50)||object.getInt("NIVEL_AGUA")<=10||object.getInt("NIVEL_AGUA2")<=10){
+                        cont.setBackgroundResource(R.drawable.orangeborder);
+                        imaget.setImageResource(R.drawable.sad);
+                        statet.setText("Estado\nLa planta necesita atención");
+
+                    }else{
+                        cont.setBackgroundResource(R.drawable.border);
+                        imaget.setImageResource(R.drawable.japy);
+
+                        statet.setText("Estado\nLa planta está bien");
+                    }
 
 
                 } catch (JSONException e) {
